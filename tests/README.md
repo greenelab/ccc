f05e78f22179fa0482b67def22470c3b6fa69293
Milton Pividori <miltondp@gmail.com>
2021-08-10 14:48:51 -0400

    add tests on basic modules

---
+++ tests/README.md
@@ -0,0 +1,13 @@
+# Unit tests
+
+## Run
+
+These are the instructions to run the unit tests. It is assumed that you already 
+followed the steps to set up the environment and download the needed data, and that
+your `PYTHONPATH` and `CM_ROOT_DIR` variables are adjusted appropriately.
+
+Execute these commands to run the unit tests:
+
+```bash
+pytest -rs --color=yes tests/
+```
---
+++ tests/test_log.py
@@ -0,0 +1,21 @@
+"""
+Tests the log.py module.
+"""
+
+
+def test_log_module_load():
+    from clustermatch import log
+    assert log is not None
+    assert log.__file__ is not None
+
+
+def test_log_get_logger():
+    from clustermatch import log
+
+    l = log.get_logger("testing")
+    assert l is not None
+    assert hasattr(l, "info")
+    assert hasattr(l, "debug")
+    assert hasattr(l, "error")
+
+    l.info("test")
