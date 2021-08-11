dbe81dc4a099b61d57d2c98e84cfaa32a81065eb
Milton Pividori <miltondp@gmail.com>
2021-08-11 04:20:45 -0400

    fix tests on windows

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -1,6 +1,8 @@
 """
 Tests the conf.py module.
 """
+import sys
+import pytest
 
 
 def test_conf_module_load():
@@ -35,6 +37,10 @@ def test_conf_entries():
     assert conf.MANUSCRIPT is not None
 
 
+@pytest.mark.skipif(
+    sys.platform.startswith("win"),
+    reason="exporting variables is only supported in non-Windows platforms",
+)
 def test_conf_export_variables():
     from pathlib import Path
     import subprocess
