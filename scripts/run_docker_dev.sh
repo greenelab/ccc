4ad4b7666f061e97fcff99e6f0d61077bc64cc12
Milton Pividori <miltondp@gmail.com>
2021-08-27 00:18:05 -0400

    run_docker_dev.sh: fixes

--- scripts/run_docker_dev.sh
+++ scripts/run_docker_dev.sh
@@ -7,14 +7,19 @@
 CODE_DIR="/home/miltondp/projects/labs/greenelab/clustermatch_repos/clustermatch-gene-expr"
 DATA_DIR="${CODE_DIR}/base"
 MANUSCRIPT_DIR="/home/miltondp/projects/labs/greenelab/clustermatch_repos/clustermatch-gene-expr-manuscript"
+N_JOBS=3
 
 # always create data directory before running Docker
 mkdir -p ${DATA_DIR}
 
+COMMAND="$@"
+
 docker run --rm \
+  -e CM_N_JOBS=${N_JOBS} \
   -v "${CODE_DIR}:/opt/code" \
   -v "${DATA_DIR}:/opt/data" \
   -v "${MANUSCRIPT_DIR}:/opt/manuscript" \
   --user "$(id -u):$(id -g)" \
   miltondp/clustermatch_gene_expr \
-  /bin/bash -c "$@"
+  /bin/bash -c "${COMMAND}"
+
