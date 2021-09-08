df37836f5701564708737b30a94e8fc21ee44993
Milton Pividori <miltondp@gmail.com>
2021-09-08 16:21:45 -0400

    add notebook to compare precomputing of parts

---
+++ nbs/04_clustermatch_profiling/py/05-compare_precomputing_of_parts.py
@@ -0,0 +1,67 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% tags=[]
+import numpy as np
+
+from clustermatch.coef import cm
+
+# %% tags=[]
+n_genes, n_samples = 100, 1000
+
+# %% tags=[]
+np.random.seed(0)
+
+# %% tags=[]
+data = np.random.rand(n_genes, n_samples)
+
+# %% tags=[]
+data.shape
+
+
+# %% [markdown] tags=[]
+# # Original implementation (`precompute_parts=False`)
+
+# %% tags=[]
+def func():
+    return cm(data, internal_n_clusters=range(2, 10 + 1), precompute_parts=False)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 20
+func()
+
+
+# %% [markdown] tags=[]
+# # Improved implementation (`precompute_parts=True`)
+
+# %% tags=[]
+def func():
+    return cm(data, internal_n_clusters=range(2, 10 + 1), precompute_parts=True)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 20
+func()
+
+# %% tags=[]
