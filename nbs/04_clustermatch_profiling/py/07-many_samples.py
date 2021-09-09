2b7f5942367dba2bb510b320cfcb0d73b2f31273
Milton Pividori <miltondp@gmail.com>
2021-09-09 00:13:11 -0400

    add notebooks to profile cases with many genes and many samples

---
+++ nbs/04_clustermatch_profiling/py/07-many_samples.py
@@ -0,0 +1,50 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% tags=[]
+import numpy as np
+
+from clustermatch.coef import cm
+
+# %% tags=[]
+n_genes, n_samples = 100, 30000
+
+# %% tags=[]
+np.random.seed(0)
+
+# %% tags=[]
+data = np.random.rand(n_genes, n_samples)
+
+# %% tags=[]
+data.shape
+
+
+# %% [markdown] tags=[]
+# # Profile
+
+# %% tags=[]
+def func():
+    return cm(data, internal_n_clusters=range(2, 10 + 1), precompute_parts=True)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 20 -T 07-cm_many_samples.txt
+func()
+
+# %%
