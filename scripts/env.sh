bc3b20230546072ea91926e4a97481fcf2bc7d68
Milton Pividori <miltondp@gmail.com>
2021-09-09 10:21:19 -0400

    env.sh: minor improvements

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,12 +8,10 @@
 # BASE_DIR is the parent directory where the code and manuscript repos are
 # located.
 BASE_DIR=/home/miltondp/projects/labs/greenelab/clustermatch_repos
-export CM_N_JOBS=2
+export CM_N_JOBS=3
 
 export CM_ROOT_DIR=${BASE_DIR}/clustermatch-gene-expr/base
 export CM_MANUSCRIPT_DIR=${BASE_DIR}/clustermatch-gene-expr-manuscript/
 
 export PYTHONPATH=${BASE_DIR}/clustermatch-gene-expr/libs/
-
-echo "now execute: conda activate"
 
