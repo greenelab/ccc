fb18b285cf16727153266aa0abe40357195f48e7
Milton Pividori <miltondp@gmail.com>
2021-09-12 22:07:27 -0400

    run_nbs_server.sh: export variables to adjust CPU core usage

--- scripts/run_nbs_server.sh
+++ scripts/run_nbs_server.sh
@@ -16,6 +16,16 @@ echo "PYTHONPATH=${PYTHONPATH}"
 # helpful if the configuration is needed outside python)
 eval `python libs/clustermatch/conf.py`
 
+# export variables related to CPU cores usage among several libraries
+if [ -z "${CM_N_JOBS}" ]; then
+  CM_N_JOBS=1
+fi
+export NUMBA_NUM_THREADS=${CM_N_JOBS}
+export MKL_NUM_THREADS=${CM_N_JOBS}
+export OPEN_BLAS_NUM_THREADS=${CM_N_JOBS}
+export NUMEXPR_NUM_THREADS=${CM_N_JOBS}
+export OMP_NUM_THREADS=${CM_N_JOBS}
+
 IP="127.0.0.1"
 TOKEN=""
 EXTRA_ARGS=""
@@ -27,5 +37,4 @@ elif [ ! -z "$1" ]; then
 	TOKEN="${1}"
 fi
 
-exec jupyter lab --ip="${IP}" --port="${PORT}" --no-browser --NotebookApp.token="${TOKEN}" ${EXTRA_ARGS}
-
+exec jupyter lab --ip="${IP}" --port="${PORT}" --no-browser --ServerApp.token="${TOKEN}" ${EXTRA_ARGS}
