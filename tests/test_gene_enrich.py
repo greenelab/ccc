f85f1640281f76613d90b0b554a0d7c0405db80c
Milton Pividori <miltondp@gmail.com>
2021-09-24 11:23:20 -0400

    [MERGE] update test_gene_enrich.py

--- tests/test_gene_enrich.py
+++ tests/test_gene_enrich.py
@@ -1,15 +1,11 @@
 import numpy as np
-
-import rpy2.robjects as ro
 from rpy2.robjects.packages import importr, data
-from rpy2.robjects import pandas2ri
-from rpy2.robjects.conversion import localconverter
 
 clusterProfiler = importr("clusterProfiler")
 
 
 def test_run_enrich_original_example():
-    # example taken from:
+    # example based on:
     # https://yulab-smu.top/biomedical-knowledge-mining-book/clusterprofiler-comparecluster.html
     gc_sample_data = data(clusterProfiler).fetch("gcSample")["gcSample"]
 
@@ -19,6 +15,7 @@ def test_run_enrich_original_example():
         all_gene_ids.update(list(r_data))
     all_gene_ids = np.array(list(all_gene_ids))
 
+    # we need a hard partition of these gene clusters
     partition = np.zeros(all_gene_ids.shape[0]) - 1
     for cluster_id, r_data in enumerate(gc_sample_data):
         gene_cluster = np.array(r_data)
@@ -26,6 +23,16 @@ def test_run_enrich_original_example():
 
     partition_clusters = np.unique(partition)
     assert len(partition_clusters) == 8
+    assert partition_clusters.min() == 0
+    assert partition_clusters.max() == 7
+
+    # check that the last cluster has the expected number of genes
+    # the others will be different, since the example of gene clusters have
+    # overlapped genes, and here we need a hard partioning of the genes
+    assert partition[partition == 7].shape[0] == 237
+
+    # run our function
+    results = run_enrich(all_gene_ids, "Some clustering id", partition, "enrichGO", "BP")
 
 
 def test_run_enrich_no_enrichment():
