8853476dfe508a6d676a9e94dc0953a1151a65d8
Milton Pividori <miltondp@gmail.com>
2021-10-01 14:07:52 -0400

    [MERGE] GO enrichment: add notebook for recount2
    * fix

--- nbs/20_gene_set_enrichment/10_recount2/py/05_00-recount2-go_enrich.py
+++ nbs/20_gene_set_enrichment/10_recount2/py/05_00-recount2-go_enrich.py
@@ -150,7 +150,7 @@ with ProcessPoolExecutor(max_workers=conf.GENERAL["N_JOBS"]) as executor, tqdm(
         similarity_matrix_filename = SIMILARITY_MATRIX_FILENAME_TEMPLATE.format(
             #             tissue=tissue,
             #             gene_sel_strategy=gene_sel_strategy,
-            corr_method=corr_method,
+            corr_method=corr_method.split("_")[0],
         )
 
         # get the universe of genes
