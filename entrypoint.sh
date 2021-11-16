674aaccad55fec7af80fbc9ba11a3f423d7941a8
Milton Pividori <miltondp@gmail.com>
2021-11-16 09:45:35 -0500

    Docker: create home directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 # Taken from here with modifications: https://pythonspeed.com/articles/activate-conda-dockerfile/
 # The --login ensures the bash configuration is loaded,
 # enabling Conda.
+
 set +eu
 conda activate clustermatch_gene_expr
 set -euo pipefail
