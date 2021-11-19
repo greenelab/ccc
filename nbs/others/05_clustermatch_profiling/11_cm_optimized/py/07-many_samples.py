8f9d339032a44904c5f009540795b8386c5799b3
Milton Pividori <miltondp@gmail.com>
2021-11-19 01:43:21 -0500

    profiling: add 11_cm_optimized to check matrix data form again

---
+++ nbs/others/05_clustermatch_profiling/11_cm_optimized/py/07-many_samples.py
@@ -0,0 +1,81 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown]
+# Clustermatch run using a larger number of samples.
+
+# %% [markdown] tags=[]
+# # Modules
+
+# %% tags=[]
+import numpy as np
+
+from clustermatch.coef import cm
+
+# %% [markdown] tags=[]
+# # Data
+
+# %% tags=[]
+n_genes, n_samples = 10, 30000
+
+# %% tags=[]
+np.random.seed(0)
+
+# %% tags=[]
+data = np.random.rand(n_genes, n_samples)
+
+# %% tags=[]
+data.shape
+
+
+# %% [markdown] tags=[]
+# # With default `internal_n_clusters`
+
+# %% tags=[]
+def func():
+    n_clust = list(range(2, 10 + 1))
+    return cm(data, internal_n_clusters=n_clust)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 50 -T 07-cm_many_samples-default_internal_n_clusters.txt
+func()
+
+
+# %% [markdown] tags=[]
+# # With reduced `internal_n_clusters`
+
+# %% tags=[]
+def func():
+    n_clust = list(range(2, 5 + 1))
+    return cm(data, internal_n_clusters=n_clust)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 50 -T 07-cm_many_samples-less_internal_n_clusters.txt
+func()
+
+# %% tags=[]
