f05ba12b6d6715c85c791f049fa60fd813fdb25f
Milton Pividori <miltondp@gmail.com>
2021-11-19 01:47:02 -0500

    [MERGE] add notebook to compare time complexity of different coefficients

--- nbs/99_manuscript/intro/py/08-time_complexity.py
+++ nbs/99_manuscript/intro/py/08-time_complexity.py
@@ -22,12 +22,23 @@
 # %% [markdown] tags=[]
 # # Modules
 
+# %% [markdown]
+# Make sure we are not using multiple cores for this comparison.
+
+# %%
+# # %env NUMBA_NUM_THREADS=1
+# # %env MKL_NUM_THREADS=1
+# # %env OPEN_BLAS_NUM_THREADS=1
+# # %env NUMEXPR_NUM_THREADS=1
+# # %env OMP_NUM_THREADS=1
+
 # %% tags=[]
-# import pandas as pd
+from timeit import timeit
 import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # from sklearn.preprocessing import minmax_scale
 
 from clustermatch import conf
@@ -38,8 +49,8 @@ from clustermatch.methods import mic, distcorr
 # # Settings
 
 # %%
-N_REPS = 100
-N_SAMPLES_LIST = [10, 50, 100, 500]
+N_REPS = 10
+N_SAMPLES_LIST = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]
 
 # %% [markdown] tags=[]
 # # Paths
@@ -55,16 +66,106 @@ OUTPUT_FIGURE_DIR.mkdir(parents=True, exist_ok=True)
 display(OUTPUT_FIGURE_DIR)
 
 # %% [markdown]
+# # Setup code
+
+# %%
+x = np.random.rand(100)
+y = np.random.rand(100)
+
+# %%
+cm(x, y)
+
+# %%
+mic(x, y)
+
+# %%
+distcorr(x, y)
+
+
+# %%
+def run_timeit(corr_func, x, y, **kwargs):
+    return timeit(lambda: corr_func(x, y), **kwargs)
+
+
+# %% [markdown]
 # # Run
 
 # %%
+n_samples_list = []
+
 cm_times = []
 mic_times = []
 distcorr_times = []
 
 # %%
 for i, n_samples in enumerate(N_SAMPLES_LIST):
-        x = np.random.rand(size=n_samples)
-        y = np.random.rand(size=n_samples)
-        
-        avl_times[i] = timeit(avl, number=n_times)
+    x = np.random.rand(n_samples)
+    y = np.random.rand(n_samples)
+
+    n_samples_list.append(n_samples)
+
+    cm_times.append(run_timeit(cm, x, y, number=N_REPS))
+    distcorr_times.append(run_timeit(distcorr, x, y, number=N_REPS))
+
+    if n_samples <= 1000:
+        mic_times.append(run_timeit(mic, x, y, number=N_REPS))
+    else:
+        mic_times.append(np.nan)
+
+# %%
+plt.title("Distance covariance performance comparison")
+plt.xlabel("Number of samples")
+plt.ylabel("Time (seconds)")
+plt.plot(N_SAMPLES_LIST, cm_times, label="cm")
+plt.plot(N_SAMPLES_LIST, mic_times, label="mic")
+plt.plot(N_SAMPLES_LIST, distcorr_times, label="distcorr")
+plt.legend()
+plt.show()
+
+# %%
+
+# %% [markdown]
+# Multiple CPU, cdist_parts parallel attempt #1
+# * it's a weird, almost no improvement
+
+# %%
+plt.title("Distance covariance performance comparison")
+plt.xlabel("Number of samples")
+plt.ylabel("Time (seconds)")
+plt.plot(N_SAMPLES_LIST, cm_times, label="cm")
+plt.plot(N_SAMPLES_LIST, mic_times, label="mic")
+plt.plot(N_SAMPLES_LIST, distcorr_times, label="distcorr")
+plt.legend()
+plt.show()
+
+# %%
+
+# %% [markdown]
+# Results with multiple CPU enabled, current clustermatch versino with no extra optimization (should be the same as single CPU):
+
+# %%
+plt.title("Distance covariance performance comparison")
+plt.xlabel("Number of samples")
+plt.ylabel("Time (seconds)")
+plt.plot(N_SAMPLES_LIST, cm_times, label="cm")
+plt.plot(N_SAMPLES_LIST, mic_times, label="mic")
+plt.plot(N_SAMPLES_LIST, distcorr_times, label="distcorr")
+plt.legend()
+plt.show()
+
+# %%
+
+# %% [markdown]
+# Results with only one CPU core:
+
+# %%
+plt.title("Distance covariance performance comparison")
+plt.xlabel("Number of samples")
+plt.ylabel("Time (seconds)")
+plt.plot(N_SAMPLES_LIST, cm_times, label="cm")
+plt.plot(N_SAMPLES_LIST, mic_times, label="mic")
+plt.plot(N_SAMPLES_LIST, distcorr_times, label="distcorr")
+plt.legend()
+plt.show()
+
+# %%
