4f216c78e389bb74dfbc78bd5dc33124dd4ddee7
Milton Pividori <miltondp@gmail.com>
2021-11-19 16:23:10 -0500

    create_docker_image.sh: add code to push image

--- scripts/create_docker_image.sh
+++ scripts/create_docker_image.sh
@@ -5,10 +5,16 @@ VERSION="dev"
 
 docker build -t miltondp/${PROJECT_NAME}:${VERSION} -t miltondp/${PROJECT_NAME}:latest .
 
-# remember to push image:
-# docker push miltondp/${PROJECT_NAME}:${VERSION}
-# docker push miltondp/${PROJECT_NAME}:latest
+read -p "'docker push' new image? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  # push version label
+  docker push miltondp/${PROJECT_NAME}:${VERSION}
 
-# update description (short 100 chars)
-# update README.md in Docker Hub
+  # push latest label
+  docker push miltondp/${PROJECT_NAME}:latest
+
+  # update description (short 100 chars)
+  # update README.md in Docker Hub
+fi
 
