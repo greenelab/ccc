e47fefadbed0bc21bc18da13016d1abb8a89a260
Milton Pividori <miltondp@gmail.com>
2021-12-17 16:13:26 -0500

    coefs comparison/general plots: add several improvements

--- nbs/99_manuscript/coefs_comp/py/05-comp_gtex_wb-general_plots.py
+++ nbs/99_manuscript/coefs_comp/py/05-comp_gtex_wb-general_plots.py
@@ -17,7 +17,7 @@
 # # Description
 
 # %% [markdown] tags=[]
-# TODO
+# It generates different general plots to compare coefficient values from Pearson, Spearman and Clustermatch, such as their distribution.
 
 # %% [markdown] tags=[]
 # # Modules
@@ -26,15 +26,11 @@
 import pandas as pd
 import numpy as np
 
-# from scipy.stats import pearsonr, spearmanr
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# from sklearn.preprocessing import minmax_scale
-
+from clustermatch.coef import cm
 from clustermatch import conf
-
-# from clustermatch.coef import cm
 
 # %% [markdown] tags=[]
 # # Settings
@@ -89,115 +85,228 @@ df.shape
 df.head()
 
 # %% [markdown]
-# # QQ-plot
+# # Histogram plot
 
 # %%
-QUANTILES = np.linspace(0, 1, 10000)
-display(QUANTILES[:10])
-display(QUANTILES[-10:])
+with sns.plotting_context("talk", font_scale=1.0):
+    fig, ax = plt.subplots(figsize=(10, 7))
 
-# %%
-quantiles_df = pd.DataFrame(
-    {
-        "clustermatch": df["clustermatch"].quantile(QUANTILES).to_numpy(),
-        "pearson": df["pearson"].quantile(QUANTILES).to_numpy(),
-        "spearman": df["spearman"].quantile(QUANTILES).to_numpy(),
-    }
-)
+    ax = sns.histplot(
+        data=df,
+        stat="density",
+        bins=100,
+        common_bins=True,
+        common_norm=False,
+        kde=True,
+        ax=ax,
+    )
+    sns.despine(ax=ax)
 
-# %%
-quantiles_df.shape
+    ax.set_xticks(np.linspace(0, 1, 10 + 1))
 
-# %%
-quantiles_df.head()
-
-# %%
-quantiles_df.describe()
-
-# %%
-fig, ax = plt.subplots(figsize=(10, 8))
-
-sns.scatterplot(
-    data=quantiles_df,
-    x="pearson",
-    y="clustermatch",
-    label="vs Pearson (abs)",
-    ax=ax,
-)
-
-sns.scatterplot(
-    data=quantiles_df,
-    x="spearman",
-    y="clustermatch",
-    label="vs Spearman (abs)",
-    ax=ax,
-)
-
-ax.set_xlabel(None)
-# if "_k2" in "clustermatch:
-# ax.set_ylabel("clustermatch (linear)")
-
-min_val = min((quantiles_df.iloc[:, 0].min(), quantiles_df.iloc[:, 1].min()))
-max_val = max((quantiles_df.iloc[:, 0].max(), quantiles_df.iloc[:, 1].max()))
-ax.plot([min_val, max_val], [min_val, max_val], "k", linewidth=0.5)
-
-# ax.set_title(f"{ENRICH_FUNC} ({PERFORMANCE_MEASURE})")
+    plt.savefig(
+        OUTPUT_FIGURE_DIR / "dist-histograms.svg",
+        bbox_inches="tight",
+        facecolor="white",
+    )
 
 # %% [markdown]
-# Pearson/Spearman values seem to be more variable in the range (0, 1) than clustermatch
+# Coefficients' values distribute very differently. Clustermatch is skewed to the left, whereas Pearson and specially Spearman seem more uniform.
 
 # %% [markdown]
-# # Density plot
+# # Cumulative histogram plot
+
+# %% [markdown]
+# I include also a cumulative histogram without specifying `bins`.
+
+# %%
+_percent = 0.70
+_coef_at_percent = df.quantile(_percent)
+display(_coef_at_percent)
 
 # %%
 with sns.plotting_context("talk", font_scale=1.1):
     fig, ax = plt.subplots(figsize=(10, 7))
 
-    for method in [x for x in df.columns if not x.endswith("_std")]:
-        sns.distplot(x=df[method], hist=True, kde=False, label=method, ax=ax)
+    ax = sns.histplot(
+        data=df,
+        element="step",
+        fill=False,
+        stat="percent",
+        common_norm=False,
+        cumulative=True,
+        legend=False,
+        ax=ax,
+    )
+    sns.despine(ax=ax)
 
-    plt.legend()
+    ax.set_xticks(np.linspace(0, 1, 10 + 1))
+    ax.set_yticks(np.linspace(0, 100, 10 + 1))
+
+    x_lim = ax.get_xlim()
+    ax.hlines(
+        y=_percent * 100,
+        xmin=x_lim[0],
+        xmax=_coef_at_percent["spearman"],
+        color="gray",
+        linestyle="dotted",
+    )
+    ax.vlines(
+        x=_coef_at_percent["clustermatch"],
+        ymin=0,
+        ymax=_percent * 100,
+        color="gray",
+        linestyle="dotted",
+    )
+    ax.vlines(
+        x=_coef_at_percent["pearson"],
+        ymin=0,
+        ymax=_percent * 100,
+        color="gray",
+        linestyle="dotted",
+    )
+    ax.vlines(
+        x=_coef_at_percent["spearman"],
+        ymin=0,
+        ymax=_percent * 100,
+        color="gray",
+        linestyle="dotted",
+    )
+
+    ax.set_xlim(x_lim)
+
+    plt.savefig(
+        OUTPUT_FIGURE_DIR / "dist-cum_histograms.svg",
+        bbox_inches="tight",
+        facecolor="white",
+    )
 
 # %% [markdown]
-# The distributions are very different. Clustermatch is skewed to the left, whereas pearson and (specially) spearman seem more uniform.
-
-# %% [markdown]
-# ## Joint plots comparing each coefficient
+# # Joint plots comparing each coefficient
 
 # %%
-sns.jointplot(
-    data=df,
-    x="pearson",
-    y="clustermatch",
-    kind="hex",
-    bins="log",
-)
+import matplotlib as mpl
+from matplotlib.colors import LogNorm
+from seaborn.distributions import _freedman_diaconis_bins
+
 
 # %%
-sns.jointplot(
-    data=df,
-    x="spearman",
-    y="clustermatch",
-    kind="hex",
-    bins="log",
-)
+def jointplot(data, x, y, bins=None):
+    """
+    Function based on Seaborn's jointplot, but without marginal plots.
+    """
+
+    grid = sns.JointGrid(
+        data=data,
+        x=x,
+        y=y,
+    )
+
+    color = "C0"
+    color_rgb = mpl.colors.colorConverter.to_rgb(color)
+    colors = [sns.utils.set_hls_values(color_rgb, l=l) for l in np.linspace(1, 0, 12)]
+    cmap = sns.palettes.blend_palette(colors, as_cmap=True)
+
+    x_bins = min(_freedman_diaconis_bins(grid.x), 50)
+    y_bins = min(_freedman_diaconis_bins(grid.y), 50)
+    gridsize = int(np.mean([x_bins, y_bins]))
+
+    joint_kws = {
+        "bins": bins,
+    }
+
+    joint_kws.setdefault("gridsize", gridsize)
+    joint_kws.setdefault("cmap", cmap)
+    joint_kws.setdefault("rasterized", True)
+
+    grid.plot_joint(
+        plt.hexbin,
+        **joint_kws,
+    )
+
+    # remove marginal axes
+    grid.ax_marg_x.set_visible(False)
+    grid.ax_marg_y.set_visible(False)
+
+    return grid
+
 
 # %%
-sns.jointplot(
-    data=df,
-    x="spearman",
-    y="pearson",
-    kind="hex",
-    bins="log",
-)
+with sns.plotting_context("talk", font_scale=1.0):
+    g = jointplot(
+        data=df,
+        x="pearson",
+        y="clustermatch",
+        bins="log",
+    )
+
+    g.savefig(
+        OUTPUT_FIGURE_DIR / "dist-clustermatch_vs_pearson.svg",
+        bbox_inches="tight",
+        dpi=300,
+        facecolor="white",
+    )
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    g = jointplot(
+        data=df,
+        x="spearman",
+        y="clustermatch",
+        bins="log",
+    )
+
+    sns.despine(ax=g.ax_joint, left=True)
+    g.ax_joint.set_yticks([])
+    g.ax_joint.set_ylabel(None)
+
+    g.savefig(
+        OUTPUT_FIGURE_DIR / "dist-clustermatch_vs_spearman.svg",
+        bbox_inches="tight",
+        dpi=300,
+        facecolor="white",
+    )
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    g = jointplot(
+        data=df,
+        x="spearman",
+        y="pearson",
+        bins="log",
+    )
+
+    g.savefig(
+        OUTPUT_FIGURE_DIR / "dist-spearman_vs_pearson.svg",
+        bbox_inches="tight",
+        dpi=300,
+        facecolor="white",
+    )
 
 # %% [markdown]
 # ## Correlations between coefficient values
+
+# %%
+from scipy.spatial.distance import squareform
 
 # %%
 df.corr()
 
 # %%
 df.corr("spearman")
+
+# %%
+_tmp = squareform(cm(df.T))
+np.fill_diagonal(_tmp, 1.0)
+
+# %%
+pd.DataFrame(
+    data=_tmp,
+    index=df.columns.copy(),
+    columns=df.columns.copy(),
+)
+
+# %% [markdown]
+# If we correlate coefficients' values with each other (using the three coefficients, as shown above), Clustermatch and Spearman agree more between them.
 
 # %%
