890176697a616abb5b0f5f739ca898d4d50a7430
Milton Pividori <miltondp@gmail.com>
2021-12-21 13:35:44 -0500

    coefs comparison/general plots: add notebook for artery tibial

---
+++ nbs/99_manuscript/coefs_comp/py/05-gtex_artery_tibial-general_plots.py
@@ -0,0 +1,165 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# It generates different general plots to compare coefficient values from Pearson, Spearman and Clustermatch, such as their distribution.
+#
+# In `Settings` below, the data set and other options (such as tissue for GTEx) are specified.
+
+# %% [markdown] tags=[]
+# # Modules
+
+# %% tags=[]
+import pandas as pd
+from scipy import stats
+import seaborn as sns
+
+from clustermatch.plots import plot_histogram, plot_cumulative_histogram, jointplot
+from clustermatch import conf
+
+# %% [markdown] tags=[]
+# # Settings
+
+# %%
+DATASET_CONFIG = conf.GTEX
+GTEX_TISSUE = "artery_tibial"
+GENE_SEL_STRATEGY = "var_pc_log2"
+
+# %%
+# this is used for the cumulative histogram
+GENE_PAIRS_PERCENT = 0.70
+
+# %% [markdown] tags=[]
+# # Paths
+
+# %% tags=[]
+assert (
+    conf.MANUSCRIPT["BASE_DIR"] is not None and conf.MANUSCRIPT["BASE_DIR"].exists()
+), "Manuscript dir not set"
+
+# %% tags=[]
+OUTPUT_FIGURE_DIR = (
+    conf.MANUSCRIPT["FIGURES_DIR"] / "coefs_comp" / f"gtex_{GTEX_TISSUE}"
+)
+OUTPUT_FIGURE_DIR.mkdir(parents=True, exist_ok=True)
+display(OUTPUT_FIGURE_DIR)
+
+# %% tags=[]
+INPUT_CORR_FILE_TEMPLATE = (
+    DATASET_CONFIG["SIMILARITY_MATRICES_DIR"]
+    / DATASET_CONFIG["SIMILARITY_MATRIX_FILENAME_TEMPLATE"]
+)
+display(INPUT_CORR_FILE_TEMPLATE)
+
+# %% tags=[]
+INPUT_FILE = DATASET_CONFIG["SIMILARITY_MATRICES_DIR"] / str(
+    INPUT_CORR_FILE_TEMPLATE
+).format(
+    tissue=GTEX_TISSUE,
+    gene_sel_strategy=GENE_SEL_STRATEGY,
+    corr_method="all",
+)
+display(INPUT_FILE)
+
+assert INPUT_FILE.exists()
+
+# %% [markdown] tags=[]
+# # Data
+
+# %%
+df = pd.read_pickle(INPUT_FILE)
+
+# %%
+df.shape
+
+# %%
+df.head()
+
+# %% [markdown] tags=[]
+# ## Data stats
+
+# %%
+df.describe().applymap(str)
+
+# %%
+# skewness
+df.apply(lambda x: stats.skew(x))
+
+# %% [markdown]
+# # Histogram plot
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    plot_histogram(df, output_dir=OUTPUT_FIGURE_DIR)
+
+# %% [markdown]
+# Coefficients' values distribute very differently. Clustermatch is skewed to the left, whereas Pearson and specially Spearman seem more uniform.
+
+# %% [markdown]
+# # Cumulative histogram plot
+
+# %% [markdown]
+# I include also a cumulative histogram without specifying `bins`.
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    plot_cumulative_histogram(df, GENE_PAIRS_PERCENT, output_dir=OUTPUT_FIGURE_DIR)
+
+# %% [markdown]
+# # Joint plots comparing each coefficient
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    jointplot(
+        data=df,
+        x="pearson",
+        y="clustermatch",
+        output_dir=OUTPUT_FIGURE_DIR,
+    )
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    x, y = "spearman", "clustermatch"
+
+    g = jointplot(
+        data=df,
+        x=x,
+        y=y,
+    )
+
+    sns.despine(ax=g.ax_joint, left=True)
+    g.ax_joint.set_yticks([])
+    g.ax_joint.set_ylabel(None)
+
+    g.savefig(
+        OUTPUT_FIGURE_DIR / f"dist-{x}_vs_{y}.svg",
+        bbox_inches="tight",
+        dpi=300,
+        facecolor="white",
+    )
+
+# %%
+with sns.plotting_context("talk", font_scale=1.0):
+    jointplot(
+        data=df,
+        x="spearman",
+        y="pearson",
+        output_dir=OUTPUT_FIGURE_DIR,
+    )
+
+# %%
