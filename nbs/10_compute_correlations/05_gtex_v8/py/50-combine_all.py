529e6aa39ffdbf5827eb52d3209c1e682d4736c0
Milton Pividori <miltondp@gmail.com>
2021-12-21 14:08:23 -0500

    notebook to combine coefficients: add tissue parameter for papermill

--- nbs/10_compute_correlations/05_gtex_v8/py/50-combine_all.py
+++ nbs/10_compute_correlations/05_gtex_v8/py/50-combine_all.py
@@ -31,10 +31,14 @@ from clustermatch import conf
 # %% [markdown] tags=[]
 # # Settings
 
-# %%
+# %% tags=["parameters"]
 DATASET_CONFIG = conf.GTEX
+# whole blood by default, but this is a parameters cells that can be changed when running papermill
 GTEX_TISSUE = "whole_blood"
 GENE_SEL_STRATEGY = "var_pc_log2"
+
+# %% tags=[]
+assert GTEX_TISSUE is not None, "Tissue not selected"
 
 # %% [markdown] tags=[]
 # # Paths
@@ -71,33 +75,33 @@ display(OUTPUT_FILE)
 # %% [markdown] tags=[]
 # ## Gene Ensembl ID -> Symbol mapping
 
-# %%
+# %% tags=[]
 gene_map = pd.read_pickle(
     DATASET_CONFIG["DATA_DIR"] / "gtex_gene_id_symbol_mappings.pkl"
 )
 
-# %%
+# %% tags=[]
 gene_map = gene_map.set_index("gene_ens_id")["gene_symbol"].to_dict()
 
-# %%
+# %% tags=[]
 assert gene_map["ENSG00000145309.5"] == "CABS1"
 
 # %% [markdown] tags=[]
 # ## Gene expression
 
-# %%
+# %% tags=[]
 data = pd.read_pickle(INPUT_GENE_EXPR_DATA_FILE)
 
-# %%
+# %% tags=[]
 data.shape
 
-# %%
+# %% tags=[]
 data.head()
 
 # %% [markdown] tags=[]
 # ## Clustermatch
 
-# %%
+# %% tags=[]
 clustermatch_df = pd.read_pickle(
     str(INPUT_CORR_FILE_TEMPLATE).format(
         tissue=GTEX_TISSUE,
@@ -106,19 +110,19 @@ clustermatch_df = pd.read_pickle(
     )
 )
 
-# %%
+# %% tags=[]
 clustermatch_df.shape
 
-# %%
+# %% tags=[]
 clustermatch_df.head()
 
-# %%
+# %% tags=[]
 assert data.index.equals(clustermatch_df.index)
 
 # %% [markdown] tags=[]
 # ## Pearson
 
-# %%
+# %% tags=[]
 pearson_df = pd.read_pickle(
     str(INPUT_CORR_FILE_TEMPLATE).format(
         tissue=GTEX_TISSUE,
@@ -127,19 +131,19 @@ pearson_df = pd.read_pickle(
     )
 )
 
-# %%
+# %% tags=[]
 pearson_df.shape
 
-# %%
+# %% tags=[]
 pearson_df.head()
 
-# %%
+# %% tags=[]
 assert data.index.equals(pearson_df.index)
 
 # %% [markdown] tags=[]
 # ## Spearman
 
-# %%
+# %% tags=[]
 spearman_df = pd.read_pickle(
     str(INPUT_CORR_FILE_TEMPLATE).format(
         tissue=GTEX_TISSUE,
@@ -148,74 +152,74 @@ spearman_df = pd.read_pickle(
     )
 )
 
-# %%
+# %% tags=[]
 spearman_df.shape
 
-# %%
+# %% tags=[]
 spearman_df.head()
 
-# %%
+# %% tags=[]
 assert data.index.equals(spearman_df.index)
 
 
 # %% [markdown] tags=[]
 # ## Merge
 
-# %%
+# %% tags=[]
 def get_upper_triag(data, k=1):
     mask = np.triu(np.ones(data.shape), k=k).astype(bool)
     return data.where(mask)
 
 
-# %%
+# %% tags=[]
 # # make sure genes match
 # clustermatch_df = clustermatch_df.loc[pearson_df.index, pearson_df.columns]
 
-# %%
+# %% tags=[]
 clustermatch_df = get_upper_triag(clustermatch_df)
 
-# %%
+# %% tags=[]
 clustermatch_df = clustermatch_df.unstack().rename_axis((None, None)).dropna()
 
-# %%
+# %% tags=[]
 clustermatch_df.shape
 
-# %%
+# %% tags=[]
 clustermatch_df.head()
 
-# %%
+# %% tags=[]
 pearson_df = get_upper_triag(pearson_df)
 
-# %%
+# %% tags=[]
 # make pearson abs
 pearson_df = pearson_df.unstack().rename_axis((None, None)).dropna().abs()
 
-# %%
+# %% tags=[]
 pearson_df.shape
 
-# %%
+# %% tags=[]
 pearson_df.head()
 
-# %%
+# %% tags=[]
 assert clustermatch_df.index.equals(pearson_df.index)
 
-# %%
+# %% tags=[]
 spearman_df = get_upper_triag(spearman_df)
 
-# %%
+# %% tags=[]
 # make spearman abs
 spearman_df = spearman_df.unstack().rename_axis((None, None)).dropna().abs()
 
-# %%
+# %% tags=[]
 spearman_df.shape
 
-# %%
+# %% tags=[]
 spearman_df.head()
 
-# %%
+# %% tags=[]
 assert clustermatch_df.index.equals(spearman_df.index)
 
-# %%
+# %% tags=[]
 df = pd.DataFrame(
     {
         "clustermatch": clustermatch_df,
@@ -224,16 +228,19 @@ df = pd.DataFrame(
     }
 ).sort_index()
 
-# %%
+# %% tags=[]
+assert not df.isna().any().any()
+
+# %% tags=[]
 df.shape
 
-# %%
+# %% tags=[]
 df.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Save
 
-# %%
+# %% tags=[]
 df.to_pickle(OUTPUT_FILE)
 
-# %%
+# %% tags=[]
