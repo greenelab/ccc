aee0779d8962acca0953cfff74e9386c5480fd4e
Milton Pividori <miltondp@gmail.com>
2021-12-23 14:44:26 -0500

    recount2full: add notebook to preprocess data

--- nbs/05_preprocessing/py/16-recount2_full-preprocess.R
+++ nbs/05_preprocessing/py/16-recount2_full-preprocess.R
@@ -27,7 +27,6 @@
 
 # %% tags=[]
 `%>%` <- dplyr::`%>%`
-library(recount)
 library(biomaRt)
 library(reticulate)
 
@@ -53,85 +52,22 @@ data.dir
 dir.create(data.dir, recursive = TRUE, showWarnings = FALSE)
 
 # %% [markdown] tags=[]
-# # Functions
+# # Load raw data
 
 # %%
-# Get RPKM value for each gene - adapted from recount package
-getRPKM <- function(rse, length_var = "bp_length", mapped_var = NULL) {
-  # Computes the RPKM value for each gene in the sample.
-  #
-  # Args:
-  #  rse: A RangedSummarizedExperiment-class object in recount package
-  #  length_var: A length 1 character vector with the column name from rowData(rse) that has
-  #              the coding length. For gene level objects from recount this is bp_length. If
-  #              NULL, then it will use width(rowRanges(rse)) which should be used for exon RSEs.
-  #  mapped_var: A length 1 character vector with the column name from colData(rse) that has
-  #              the number of reads mapped. If NULL (default) then it will use the column
-  #              sums of the counts matrix
-  # Returns:
-  #   RPKM value for each sample
-  if (!is.null(mapped_var)) {
-    mapped <- colData(rse)[, mapped_var]
-  } else {
-    mapped <- colSums(assays(rse)$counts)
-  }
-  bg <- matrix(mapped, ncol = ncol(rse), nrow = nrow(rse), byrow = TRUE)
-  if (!is.null(length_var)) {
-    len <- rowData(rse)[, length_var]
-  } else {
-    len <- width(rowRanges(rse))
-  }
-  wid <- matrix(len, nrow = nrow(rse), ncol = ncol(rse), byrow = FALSE)
-  rpkm <- assays(rse)$counts / (wid / 1000) / (bg / 1e6)
-  return(rpkm)
-}
-
-# %% [markdown] tags=[]
-# # Download
+input.file <- file.path(recount2full.data.dir, "recount2_rpkm_raw.rds")
 
 # %%
-# Get all samples from recount database
-metasample.sra <- all_metadata(subset = "sra", verbose = TRUE)
-metasample.sra <- as.data.frame(metasample.sra)
+input.file
 
 # %%
-# Remove samples without description
-metadata.nonempty <- metasample.sra[!is.na(metasample.sra$characteristics), ]
-included.sample.list <- unique(metadata.nonempty$project)
+rpkm.df <- readRDS(input.file)
 
 # %%
-# Download all recount2 samples in included.sample.list
-lapply(
-  included.sample.list,
-  function(x) {
-    download_study(x,
-      type = "rse-gene",
-      outdir = file.path(data.dir, x)
-    )
-  }
-)
-
-# %% [markdown] tags=[]
-# # Normalize with RPKM
+dim(rpkm.df)
 
 # %%
-# get RPKM for each experiment and add to list
-rpkm.list <- list()
-for (experiment in included.sample.list) {
-  load(file.path(data.dir, experiment, "rse_gene.Rdata"))
-  rpkm <- as.data.frame(getRPKM(rse_gene))
-  rpkm$id <- rownames(rpkm)
-  rpkm.list[[experiment]] <- rpkm
-}
-
-# %%
-# combine experiments -- this is the most memory efficient way to go about this
-# that I've found -- will need to drop extraneous gene id columns
-rpkm.df <- do.call(base::cbind, c(rpkm.list, by = "id"))
-rpkm.df <- rpkm.df %>% dplyr::select(-dplyr::ends_with("id"))
-rpkm.df <- tibble::rownames_to_column(rpkm.df, "ENSG")
-# drop last column "by" -- information about what was used with base::cbind
-rpkm.df <- rpkm.df %>% dplyr::select(-by)
+head(rpkm.df[, 1:10])
 
 # %% [markdown] tags=[]
 # # Preprocess data
@@ -142,11 +78,15 @@ mart <- biomaRt::useDataset(
   "hsapiens_gene_ensembl",
   biomaRt::useMart("ensembl")
 )
+
+# %%
 genes <- unlist(lapply(strsplit(rpkm.df$ENSG, "[.]"), `[[`, 1))
+
 rpkm.df$ensembl_gene_id <- unlist(lapply(
   strsplit(rpkm.df$ENSG, "[.]"),
   `[[`, 1
 ))
+
 gene.df <- biomaRt::getBM(
   filters = "ensembl_gene_id",
   attributes = c("ensembl_gene_id", "hgnc_symbol"),
@@ -221,4 +161,12 @@ saveRDS(rpkm.df, file = paste0(output_filepath, ".rds"))
 # %%
 py_save_object(rpkm.df, paste0(output_filepath, ".pkl"))
 
+# %% [markdown] tags=[]
+# # Cleanup
+
 # %%
+# the raw file is not longer necessary
+if (file.exists(input.file)) {
+  # Delete file if it exists
+  file.remove(input.file)
+}
