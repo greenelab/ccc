fe437a8f9f562f0ffce0812cdefc17a0661308fc
Milton Pividori <miltondp@gmail.com>
2021-12-23 14:24:16 -0500

    convert_ipynb_to_py.sh: select code formatter according to language

--- scripts/convert_ipynb_to_py.sh
+++ scripts/convert_ipynb_to_py.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# show commands being executed (for debugging purposes)
+#set -x
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 NOTEBOOK="${1}"
 if [ -z "${NOTEBOOK}" ]; then
   echo "Provide the notebook path"
   exit 1
 fi
 
+# capture whether notebook has a python or R kernel
+regex="\"file_extension\": \"(\.[a-zA-Z]+)\"\,"
+value=`cat ${NOTEBOOK} | grep "file_extension"`
+if [[ $value =~ $regex ]]; then
+  fext="${BASH_REMATCH[1]}"
+else
+  echo "ERROR: file extension not found"
+  exit 1
+fi
+
+# select code formatter according to file extension
+PIPE_CMD=("black {}")
+if [ "$fext" = ".r" ]; then
+  PIPE_CMD=("${SCRIPT_DIR}/styler.r {}")
+fi
+
 jupytext \
   --sync \
-  --pipe black \
+  --pipe "${PIPE_CMD[@]}" \
   ${NOTEBOOK}
 
-RESULT=$?
-if [ $RESULT -ne 0 ]; then
-    # if jupytext failed, it is very likely because it was an R script.
-    # Try again without black
-    jupytext \
-      --sync \
-      ${NOTEBOOK}
-fi
