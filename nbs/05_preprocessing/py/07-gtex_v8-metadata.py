32dfcde0e994dd0f2cf11a3df4b4b362de1ff539
Milton Pividori <miltondp@gmail.com>
2022-01-24 15:13:28 -0500

    preprocessing: run notebooks with papermill

--- nbs/05_preprocessing/py/07-gtex_v8-metadata.py
+++ nbs/05_preprocessing/py/07-gtex_v8-metadata.py
@@ -35,114 +35,114 @@ from clustermatch import conf
 # %% [markdown] tags=[]
 # # Paths
 
-# %%
+# %% tags=[]
 TISSUE_DIR = conf.GTEX["DATA_DIR"] / "data_by_tissue"
 assert TISSUE_DIR.exists()
 
-# %%
+# %% tags=[]
 OUTPUT_DIR = conf.GTEX["DATA_DIR"]
 display(OUTPUT_DIR)
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Data
 
 # %% [markdown] tags=[]
 # ## GTEx samples info
 
-# %%
+# %% tags=[]
 assert conf.GTEX["SAMPLE_ATTRS_FILE"].exists(), "Sample files does not exist"
 
-# %%
+# %% tags=[]
 gtex_samples = pd.read_csv(
     conf.GTEX["SAMPLE_ATTRS_FILE"],
     sep="\t",
     index_col="SAMPID",
 )
 
-# %%
+# %% tags=[]
 display(gtex_samples.shape)
 assert gtex_samples.index.is_unique
 
-# %%
+# %% tags=[]
 gtex_samples.head()
 
 # %% [markdown] tags=[]
 # ## GTEx subject phenotypes
 
-# %%
+# %% tags=[]
 assert conf.GTEX["SUBJECTS_ATTRS_FILE"].exists(), "Subject files does not exist"
 
-# %%
+# %% tags=[]
 gtex_phenotypes = pd.read_csv(
     conf.GTEX["SUBJECTS_ATTRS_FILE"],
     sep="\t",
 )
 
-# %%
+# %% tags=[]
 gtex_phenotypes.shape
 
-# %%
+# %% tags=[]
 gtex_phenotypes.head()
 
 # %% [markdown] tags=[]
 # ## GTEx gene expression sample
 
-# %%
+# %% tags=[]
 pd.read_pickle(next(TISSUE_DIR.glob("*.pkl"))).head()
 
 # %% [markdown] tags=[]
 # ## Gene Ensembl ID -> Symbol mapping
 
-# %%
+# %% tags=[]
 gene_map = pd.read_pickle(conf.GTEX["DATA_DIR"] / "gtex_gene_id_symbol_mappings.pkl")
 
-# %%
+# %% tags=[]
 gene_map = gene_map.set_index("gene_ens_id")["gene_symbol"].to_dict()
 
-# %%
+# %% tags=[]
 assert gene_map["ENSG00000145309.5"] == "CABS1"
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Get GTEx sample metadata
 
-# %%
+# %% tags=[]
 gtex_samples_ids = gtex_samples.index.to_list()
 display(gtex_samples_ids[:5])
 
-# %%
+# %% tags=[]
 gtex_samples_ids = pd.Series(gtex_samples_ids).rename("SAMPID")
 
-# %%
+# %% tags=[]
 gtex_samples_ids
 
-# %%
+# %% tags=[]
 gtex_subjects_ids = gtex_samples_ids.str.extract(
     r"([\w\d]+\-[\w\d]+)", flags=re.IGNORECASE, expand=True
 )[0].rename("SUBJID")
 
-# %%
+# %% tags=[]
 gtex_subjects_ids
 
-# %%
+# %% tags=[]
 gtex_metadata = pd.concat([gtex_samples_ids, gtex_subjects_ids], axis=1)
 
-# %%
+# %% tags=[]
 gtex_metadata
 
-# %%
+# %% tags=[]
 gtex_phenotypes
 
-# %%
+# %% tags=[]
 gtex_metadata = pd.merge(gtex_metadata, gtex_phenotypes).set_index("SAMPID")
 
-# %%
+# %% tags=[]
 gtex_metadata
 
-# %%
+# %% tags=[]
 gtex_metadata = pd.merge(gtex_metadata, gtex_samples, left_index=True, right_index=True)
 
-# %%
+# %% tags=[]
 gtex_metadata = gtex_metadata.replace(
     {
         "SEX": {
@@ -152,38 +152,38 @@ gtex_metadata = gtex_metadata.replace(
     }
 )
 
-# %%
+# %% tags=[]
 gtex_metadata = gtex_metadata.sort_index()
 
-# %%
+# %% tags=[]
 gtex_metadata.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Testing
 
-# %%
+# %% tags=[]
 gtex_metadata.shape
 
-# %%
+# %% tags=[]
 assert not gtex_metadata["SUBJID"].isna().any()
 
-# %%
+# %% tags=[]
 assert not gtex_metadata["SMTS"].isna().any()
 assert not gtex_metadata["SMTSD"].isna().any()
 
-# %%
+# %% tags=[]
 assert not gtex_metadata["SEX"].isna().any()
 assert gtex_metadata["SEX"].unique().shape[0] == 2
 assert set(gtex_metadata["SEX"].unique()) == {"Female", "Male"}
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Save
 
-# %%
+# %% tags=[]
 output_filename = OUTPUT_DIR / "gtex_v8-sample_metadata.pkl"
 display(output_filename)
 
-# %%
+# %% tags=[]
 gtex_metadata.to_pickle(output_filename)
 
-# %%
+# %% tags=[]
