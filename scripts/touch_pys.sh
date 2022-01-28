09a04fa1eda9a6e2b4b527639331243f49b6a544
Milton Pividori <miltondp@gmail.com>
2022-01-28 16:07:40 -0500

    touch_pys.sh: improve documentation

--- scripts/touch_pys.sh
+++ scripts/touch_pys.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Executes all python scripts (.py) converted from notebooks to update their
-# access date. This is needed sometimes when git updates files after a push,
-# otherwise jupyter won't load the notebooks in the browser.
+# Updates the access date of all python scripts (.py) converted from notebooks.
+# This is needed sometimes when git updates files after a pull, otherwise
+# jupyter won't load the notebooks in the browser.
 
 find . -type f -wholename "**/py/*.py" -exec touch {} +
-
