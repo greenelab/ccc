5884ff11eb7a145d51c0e1ad68eb86079687f594
Milton Pividori <miltondp@gmail.com>
2022-04-08 11:59:33 -0400

    results/time_test: add MIC_e

--- libs/clustermatch/methods.py
+++ libs/clustermatch/methods.py
@@ -6,13 +6,13 @@ import warnings
 from minepy.mine import MINE
 
 
-def mic(x, y):
+def mic(x, y, estimator="mic_approx"):
     """
     Given two arrays (x and y), it computes MIC with the default parameters.
     """
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        mine = MINE(alpha=0.6, c=15, est="mic_approx")
+        mine = MINE(alpha=0.6, c=15, est=estimator)
         mine.compute_score(x, y)
         return mine.mic()
