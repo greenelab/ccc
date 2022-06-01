7847f0042fe2bbde8a20f6e3dc9b561a2d5bd985
Milton Pividori <miltondp@gmail.com>
2022-06-01 16:40:21 -0400

    rsync.sh: add some improvements
    Co-authored-by: Ariel Hippen <ariel.hippen@gmail.com>

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -6,12 +6,21 @@ set -e
 # This script runs rsync with some common parameters to sync with a remote
 # machine. For instance, it checks files' hashes instead of timestamp, and
 # excludes some huge files not needed.
+#
+# It accepts one argument, and it is the remote directory path (absolute) where
+# the base directory is.
 
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 LOCAL_DIR="${GIT_ROOT_DIR}/base/"
 
-# TODO directory should be [...]/projects/ccc/ccc/base
-REMOTE_DIR="/home/miltondp/projects/labs/greenelab/clustermatch_repos/clustermatch-gene-expr/base/*"
+REMOTE_DIR="${1}"
+if [ -z "${REMOTE_DIR}" ]; then
+  # if remote dir not given, use the same as local
+  REMOTE_DIR=${LOCAL_DIR}
+else
+  # default value
+  REMOTE_DIR="/home/miltondp/projects/ccc/ccc/base/*"
+fi
 
 rsync \
         -chavzP \
