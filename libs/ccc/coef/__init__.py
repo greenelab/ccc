9c36efbe0c837fb9a99489d04cea2f18688c19a4
Milton Pividori <miltondp@gmail.com>
2022-06-21 14:18:40 -0400

    setup.py: add file to create PyPI package

--- libs/ccc/coef/__init__.py
+++ libs/ccc/coef/__init__.py
@@ -1,1 +1,7 @@
 from ccc.coef.impl import *  # noqa: F403, F401
+
+try:
+    # Run CCC to initialize/compile its functions with numba
+    ccc(np.random.rand(10), np.random.rand(10))
+except Exception as e:
+    raise e
