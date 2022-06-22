273a02385a33fc02570b8d4079b425e7f5a4db03
Milton Pividori <miltondp@gmail.com>
2022-06-22 15:24:10 -0400

    attempt to fix flake8 errors

--- libs/ccc/coef/__init__.py
+++ libs/ccc/coef/__init__.py
@@ -2,6 +2,9 @@ from ccc.coef.impl import *  # noqa: F403, F401
 
 try:
     # Run CCC to initialize/compile its functions with numba
+    from ccc.coef.impl import ccc
+    import numpy as np
+
     ccc(np.random.rand(10), np.random.rand(10))
 except Exception as e:
     raise e
