701dd0c47e4cd575d2ebeadf9e26ed85e1c6d218
Milton Pividori <miltondp@gmail.com>
2022-06-24 13:17:49 -0400

    ccc/coef/__init__.py: simplify code

--- libs/ccc/coef/__init__.py
+++ libs/ccc/coef/__init__.py
@@ -1,10 +1,7 @@
 from ccc.coef.impl import *  # noqa: F403, F401
 
-try:
-    # Run CCC to initialize/compile its functions with numba
-    from ccc.coef.impl import ccc
-    import numpy as np
+# Run CCC to initialize/compile its functions with numba
+from ccc.coef.impl import ccc
+import numpy as np
 
-    ccc(np.random.rand(10), np.random.rand(10))
-except Exception as e:
-    raise e
+ccc(np.random.rand(10), np.random.rand(10))
