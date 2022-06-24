d90e774ba694f30b3d3d3db81bc9968409c3292f
Milton Pividori <miltondp@gmail.com>
2022-06-24 12:36:56 -0400

    run_nbs_server.sh: fixes and simplify code

--- scripts/run_nbs_server.sh
+++ scripts/run_nbs_server.sh
@@ -9,32 +9,23 @@
 #   in addition, it will listen to any address (*).
 
 PORT=8893
-export PYTHONPATH=`pwd`/libs/
-echo "PYTHONPATH=${PYTHONPATH}"
-
-# export the configuration as environmental variables (this is
-# helpful if the configuration is needed outside python)
-eval `python libs/clustermatch/conf.py`
-
-# export variables related to CPU cores usage among several libraries
-if [ -z "${CM_N_JOBS}" ]; then
-  CM_N_JOBS=1
-fi
-export NUMBA_NUM_THREADS=${CM_N_JOBS}
-export MKL_NUM_THREADS=${CM_N_JOBS}
-export OPEN_BLAS_NUM_THREADS=${CM_N_JOBS}
-export NUMEXPR_NUM_THREADS=${CM_N_JOBS}
-export OMP_NUM_THREADS=${CM_N_JOBS}
 
 IP="127.0.0.1"
 TOKEN=""
 EXTRA_ARGS=""
+
 if [ "$1" = "--container-mode" ]; then
-    IP="*"
-    EXTRA_ARGS="--allow-root"
+  IP="*"
+#  EXTRA_ARGS="--allow-root"
 elif [ ! -z "$1" ]; then
-	IP="*"
-	TOKEN="${1}"
+  IP="*"
+  TOKEN="${1}"
 fi
 
-exec jupyter lab --ip="${IP}" --port="${PORT}" --no-browser --ServerApp.token="${TOKEN}" ${EXTRA_ARGS}
+exec jupyter lab \
+  --ip="${IP}" \
+  --port="${PORT}" \
+  --ContentsManager.allow_hidden=True \
+  --no-browser \
+  --ServerApp.token="${TOKEN}" ${EXTRA_ARGS}
+
