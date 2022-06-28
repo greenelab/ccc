74611ee08c5211e1374637d581a37201ec6c68b2
Milton Pividori <miltondp@gmail.com>
2022-06-28 12:29:43 -0400

    ismb2022: add time test notebook

---
+++ nbs/others/ismb2022/py/07-time_test-plot.py
@@ -0,0 +1,247 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# This is the same notebook in `nbs/99_manuscript` but adapted for a poster at ISMB 2022.
+#
+# Make plots to show the computational complexity results comparing all coefficients.
+
+# %% [markdown] tags=[]
+# # Modules loading
+
+# %% tags=[]
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from ccc import conf
+
+# %% [markdown] tags=[]
+# # Settings
+
+# %%
+INPUT_FILENAME_TEMPLATE = "time_test"
+
+# %% [markdown] tags=[]
+# # Paths
+
+# %% tags=[]
+assert (
+    conf.MANUSCRIPT["BASE_DIR"] is not None and conf.MANUSCRIPT["BASE_DIR"].exists()
+), "Manuscript dir not set"
+
+# %% tags=[]
+COEF_COMP_DIR = conf.MANUSCRIPT["FIGURES_DIR"] / "coefs_comp"
+COEF_COMP_DIR.mkdir(parents=True, exist_ok=True)
+display(COEF_COMP_DIR)
+
+# %%
+OUTPUT_FIGURE_DIR = conf.MANUSCRIPT["FIGURES_DIR"] / "ismb2022" / "poster_figures"
+OUTPUT_FIGURE_DIR.mkdir(parents=True, exist_ok=True)
+display(OUTPUT_FIGURE_DIR)
+
+# %% tags=[]
+INPUT_DIR = conf.RESULTS_DIR / "time_test"
+display(INPUT_DIR)
+
+# %% [markdown] tags=[]
+# # Load results
+
+# %%
+time_results = pd.read_pickle(INPUT_DIR / f"{INPUT_FILENAME_TEMPLATE}.pkl")
+
+# %%
+time_results.shape
+
+# %%
+time_results.head()
+
+# %%
+time_results["method"].unique()
+
+# %% [markdown] tags=[]
+# # Processing
+
+# %%
+time_results = time_results.replace(
+    {
+        "method": {
+            "p-1": "Pearson (1 core)",
+            "s-1": "Spearman (1 core)",
+            "cm-1": "CCC (1 core)",
+            "mic-1": "MIC (1 core)",
+            "mic_e-1": "MICe (1 core)",
+            "p-3": "Pearson (3 cores)",
+            "s-3": "Spearman (3 cores)",
+            "cm-3": "CCC (3 cores)",
+            "mic-3": "MIC (3 cores)",
+            "mic_e-3": "MICe (3 cores)",
+        }
+    }
+)
+
+# %%
+time_results.shape
+
+# %%
+time_results.head()
+
+# %% [markdown]
+# # Run numbers
+
+# %%
+plot_data = time_results
+
+# %%
+run_numbers = (
+    plot_data[
+        plot_data["method"].str.contains("1 core", regex=False)
+        | plot_data["method"].str.contains("CCC (3 cores)", regex=False)
+    ]
+    .groupby(["data_size", "method"])["time"]
+    .describe()
+)
+display(run_numbers)
+
+# %% [markdown]
+# # Plot
+
+# %%
+hue_order = sorted(time_results["method"].unique())
+
+# %%
+hue_order
+
+# %%
+deep_colors = sns.color_palette("muted")
+display(deep_colors)
+
+
+# %%
+def format_data_size(x):
+    if x < 1000:
+        return f"{int(x)}"
+    elif x < 1000000:
+        return f"{int(x/1000)}k"
+
+    return f"{int(x/1000000)}m"
+
+
+plot_data = plot_data.assign(data_size=plot_data["data_size"].apply(format_data_size))
+
+# %% [markdown]
+# ## First analysis
+
+# %% [markdown]
+# Here I take a look if using more than 1 core benefits methods.
+
+# %%
+with sns.plotting_context("paper", font_scale=1.5):
+    g = sns.catplot(
+        kind="point",
+        data=plot_data,
+        x="data_size",
+        y="time",
+        hue="method",
+        hue_order=hue_order,
+        palette=deep_colors,
+        height=5,
+        aspect=1.4,
+    )
+
+    plt.xlabel("Number of measured objects")
+    plt.ylabel("Time (seconds)")
+
+# %%
+with sns.plotting_context("paper", font_scale=1.5):
+    g = sns.catplot(
+        kind="point",
+        data=plot_data,
+        x="data_size",
+        y="time",
+        hue="method",
+        hue_order=hue_order,
+        palette=deep_colors,
+        height=5,
+        aspect=1.4,
+    )
+
+    plt.xlabel("Number of measured objects")
+    plt.ylabel("Time (seconds) in log scale")
+
+    g.ax.set_yscale("log")
+
+# %% [markdown]
+# Only CCC is really taking advantage of more than 1 core, so I'll remove the rest below.
+
+# %% [markdown]
+# # Final analysis
+
+# %%
+# select runs with 3 cores for the other methods
+plot_data = plot_data.replace(
+    {
+        "method": {
+            "Pearson (3 cores)": "Pearson",
+            "Spearman (3 cores)": "Spearman",
+            "MIC (3 cores)": "MIC",
+            "MICe (3 cores)": r"$\mathregular{MIC_e}$",
+        }
+    }
+)
+
+# %%
+plot_data["method"].unique()
+
+# %%
+hue_order = [
+    # "MIC",
+    r"$\mathregular{MIC_e}$",
+    # "CCC (1 core)",
+    "CCC (3 cores)",
+    "Spearman",
+    "Pearson",
+]
+
+# %%
+with sns.plotting_context("poster", font_scale=1.3):
+    g = sns.catplot(
+        kind="point",
+        data=plot_data,
+        x="data_size",
+        y="time",
+        hue="method",
+        hue_order=hue_order,
+        palette=deep_colors,
+        height=8,
+        aspect=1.30,
+        legend=False,
+    )
+
+    plt.legend(loc="best", fontsize="small", framealpha=0.5)
+    plt.xlabel("Number of measured objects")
+    plt.ylabel("Time (seconds) in log scale")
+    g.ax.set_yscale("log")
+
+    plt.savefig(
+        OUTPUT_FIGURE_DIR / f"{INPUT_FILENAME_TEMPLATE}-log.pdf",
+        bbox_inches="tight",
+        facecolor="white",
+    )
+
+# %%
