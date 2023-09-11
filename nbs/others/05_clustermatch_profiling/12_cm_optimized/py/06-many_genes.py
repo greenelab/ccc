0f251566702fa3ca6b326c5a1e7b7f92d9005336
Milton Pividori <miltondp@gmail.com>
2023-09-10 23:13:47 -0600

    add profiling results with the latest implementation

---
+++ nbs/others/05_clustermatch_profiling/12_cm_optimized/py/06-many_genes.py
@@ -0,0 +1,82 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# Clustermatch run using a larger number of genes.
+
+# %% [markdown] tags=[]
+# # Remove pycache dir
+
+# %% tags=[]
+# !echo ${CODE_DIR}
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -print
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -exec rm -rf {} \;
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -print
+
+# %% [markdown] tags=[]
+# # Modules
+
+# %% tags=[]
+import numpy as np
+
+from ccc.coef import ccc
+
+# %% tags=[]
+# let numba compile all the code before profiling
+ccc(np.random.rand(10), np.random.rand(10))
+
+# %% [markdown] tags=[]
+# # Data
+
+# %% tags=[]
+n_genes, n_samples = 500, 1000
+
+# %% tags=[]
+np.random.seed(0)
+
+# %% tags=[]
+data = np.random.rand(n_genes, n_samples)
+
+# %% tags=[]
+data.shape
+
+
+# %% [markdown] tags=[]
+# # Profile
+
+# %% tags=[]
+def func():
+    n_clust = list(range(2, 10 + 1))
+    return ccc(data, internal_n_clusters=n_clust)
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 50 -T 06-cm_many_genes.txt
+func()
+
+# %% tags=[]
---
+++ nbs/others/05_clustermatch_profiling/12_cm_optimized/py/11-many_samples.py
@@ -0,0 +1,112 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# Similar as `06` but it computes across gene pairs instead of data matrix.
+
+# %% [markdown] tags=[]
+# # Remove pycache dir
+
+# %% tags=[]
+# !echo ${CODE_DIR}
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -print
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -prune -exec rm -rf {} \;
+
+# %% tags=[]
+# !find ${CODE_DIR}/libs -regex '^.*\(__pycache__\)$' -print
+
+# %% [markdown] tags=[]
+# # Modules
+
+# %% tags=[]
+import numpy as np
+
+from ccc.coef import ccc
+
+# %% tags=[]
+# let numba compile all the code before profiling
+ccc(np.random.rand(10), np.random.rand(10))
+
+# %% [markdown] tags=[]
+# # Data
+
+# %% tags=[]
+n_genes, n_samples = 10, 30000
+
+# %% tags=[]
+np.random.seed(0)
+
+# %% tags=[]
+data = np.random.rand(n_genes, n_samples)
+
+# %% tags=[]
+data.shape
+
+
+# %% [markdown] tags=[]
+# # With default `internal_n_clusters`
+
+# %% tags=[]
+def func():
+    res = np.full(int((data.shape[0] * (data.shape[0] - 1)) / 2), np.nan)
+
+    n_clust = list(range(2, 10 + 1))
+    idx = 0
+    for i in range(data.shape[0] - 1):
+        for j in range(i + 1, data.shape[0]):
+            res[idx] = ccc(data[i], data[j], internal_n_clusters=n_clust)
+            idx += 1
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 50 -T 11-cm_many_samples-default_internal_n_clusters.txt
+func()
+
+
+# %% [markdown] tags=[]
+# # With reduced `internal_n_clusters`
+
+# %% tags=[]
+def func():
+    res = np.full(int((data.shape[0] * (data.shape[0] - 1)) / 2), np.nan)
+
+    n_clust = list(range(2, 5 + 1))
+    idx = 0
+    for i in range(data.shape[0] - 1):
+        for j in range(i + 1, data.shape[0]):
+            res[idx] = ccc(data[i], data[j], internal_n_clusters=n_clust)
+            idx += 1
+
+
+# %% tags=[]
+# %%timeit func()
+func()
+
+# %% tags=[]
+# %%prun -s cumulative -l 50 -T 11-cm_many_samples-less_internal_n_clusters.txt
+func()
+
+# %% tags=[]
