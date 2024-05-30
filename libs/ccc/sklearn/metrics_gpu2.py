cf21b48d3e5dd6462657ea83f7e7621dcbaeb292
Haoyu Zhang <haoyu_z@outlook.com>
2024-05-30 13:21:21 -0600

    [test]: Create a new jupyter testing folder

---
+++ libs/ccc/sklearn/metrics_gpu2.py
@@ -0,0 +1,210 @@
+"""
+Contains implementations of different metrics in sklearn but optimized for numba.
+
+Some code (indicated in each function) is based on scikit-learn's code base
+(https://github.com/scikit-learn), for which the copyright notice and license
+are shown below.
+
+BSD 3-Clause License
+
+Copyright (c) 2007-2021 The scikit-learn developers.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import numpy as np
+from numba import njit
+from numba import cuda
+
+@njit(cache=True, nogil=True)
+def get_contingency_matrix(part0: np.ndarray, part1: np.ndarray) -> np.ndarray:
+    """
+    Given two clustering partitions with k0 and k1 number of clusters each, it
+    returns a contingency matrix with k0 rows and k1 columns. It's an implementation of
+    https://scikit-learn.org/stable/modules/generated/sklearn.metrics.cluster.contingency_matrix.html,
+    but the code is not based on their implementation.
+
+    Args:
+        part0: a 1d array with cluster assignments for n objects.
+        part1: a 1d array with cluster assignments for n objects.
+
+    Returns:
+        A contingency matrix with k0 (number of clusters in part0) rows and k1
+        (number of clusters in part1) columns. Each cell ij represents the
+        number of objects grouped in cluster i (in part0) and cluster j (in
+        part1).
+    """
+    part0_unique = np.unique(part0)
+    part1_unique = np.unique(part1)
+
+    cont_mat = np.zeros((len(part0_unique), len(part1_unique)))
+
+    for i in range(len(part0_unique)):
+        part0_k = part0_unique[i]
+
+        for j in range(len(part1_unique)):
+            part1_k = part1_unique[j]
+
+            part0_i = part0 == part0_k
+            part1_j = part1 == part1_k
+
+            cont_mat[i, j] = np.sum(part0_i & part1_j)
+
+    return cont_mat
+
+
+@njit(cache=True, nogil=True)
+def get_pair_confusion_matrix(part0: np.ndarray, part1: np.ndarray) -> np.ndarray:
+    """
+    Returns the pair confusion matrix from two clustering partitions. It is an
+    implemenetation of
+    https://scikit-learn.org/stable/modules/generated/sklearn.metrics.cluster.pair_confusion_matrix.html
+    The code is based on the sklearn implementation. See copyright notice at the
+    top of this file.
+
+    Args:
+        part0: a 1d array with cluster assignments for n objects.
+        part1: a 1d array with cluster assignments for n objects.
+
+    Returns:
+        A pair confusion matrix with 2 rows and 2 columns. From sklearn's
+        pair_confusion_matrix docstring: considering a pair of objects that is
+        clustered together a positive pair, then as in binary classification the
+        count of true negatives is in position 00, false negatives in 10, true
+        positives in 11, and false positives in 01.
+    """
+    n_samples = np.int64(part0.shape[0])
+
+    # Computation using the contingency data
+    contingency = get_contingency_matrix(part0, part1)
+    n_c = np.ravel(contingency.sum(axis=1))
+    n_k = np.ravel(contingency.sum(axis=0))
+    sum_squares = (contingency**2).sum()
+    C = np.empty((2, 2), dtype=np.int64)
+    C[1, 1] = sum_squares - n_samples
+    C[0, 1] = contingency.dot(n_k).sum() - sum_squares
+    C[1, 0] = contingency.transpose().dot(n_c).sum() - sum_squares
+    C[0, 0] = n_samples**2 - C[0, 1] - C[1, 0] - sum_squares
+    return C
+
+
+def adjusted_rand_index(part0: np.ndarray, part1: np.ndarray) -> float:
+    """
+    Computes the adjusted Rand index (ARI) between two clustering partitions.
+    The code is based on the sklearn implementation here:
+    https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html
+    See copyright notice at the top of this file.
+
+    This function should not be compiled with numba, since it depends on
+    arbitrarily large interger variable (supported by Python) to correctly
+    compute the ARI in large partitions.
+
+    Args:
+        part0: a 1d array with cluster assignments for n objects.
+        part1: a 1d array with cluster assignments for n objects.
+
+    Returns:
+        A number representing the adjusted Rand index between two clustering
+        partitions. This number is between something around 0 (partitions do not
+        match; it could be negative in some cases) and 1.0 (perfect match).
+    """
+    (tn, fp), (fn, tp) = get_pair_confusion_matrix(part0, part1)
+    # convert to Python integer types, to avoid overflow or underflow
+    tn, fp, fn, tp = int(tn), int(fp), int(fn), int(tp)
+
+    # Special cases: empty data or full agreement
+    if fn == 0 and fp == 0:
+        return 1.0
+
+    return 2.0 * (tp * tn - fn * fp) / ((tp + fn) * (fn + tn) + (tp + fp) * (fp + tn))
+
+
+@cuda.jit
+def increment_by_one(an_array):
+    # Thread id in a 1D block
+    tx = cuda.threadIdx.x
+    # Block id in a 1D grid
+    ty = cuda.blockIdx.x
+    # Block width, i.e. number of threads per block
+    bw = cuda.blockDim.x
+    # Compute flattened index inside the array
+    pos = tx + ty * bw
+    if pos < an_array.size:  # Check array boundaries
+        an_array[pos] += 1
+
+def _test_cuda1():
+    # Initialize the array
+    data = np.ones(64)
+    print(f"Data before kernel call: {data}")
+    # Set the number of threads in a block
+    threads_per_block = 32
+    # Calculate the number of thread blocks in the grid
+    blocks_per_grid = (data.size + (threads_per_block - 1)) // threads_per_block
+    # Call the kernel
+    increment_by_one[blocks_per_grid, threads_per_block](data)
+    print(f"Data after kernel call: {data}")
+    return
+
+
+def _test_ari():
+    part0 = np.array([0, 0, 1, 1, 2, 2])
+    part1 = np.array([0, 0, 1, 1, 2, 2])
+    print(adjusted_rand_index(part0, part1))  # 1.0
+
+    part0 = np.array([0, 0, 1, 1])
+    part1 = np.array([0, 0, 1, 2])
+    print(adjusted_rand_index(part0, part1))  # 0.57
+
+    part0 = np.array([0, 0, 1, 1])
+    part1 = np.array([0, 1, 0, 1])
+    print(adjusted_rand_index(part0, part1))  # -0.5
+
+
+def print_device_info():
+    # Get the current device
+    device = cuda.get_current_device()
+    print(dir(device))
+    # Print device information
+    print("Device Information:")
+    print(f"Device ID: {device.id}")
+    print(f"Name: {device.name}")
+    # print(f"Total Memory: {device.total_memory / (1024 ** 3):.2f} GB")
+    print(f"Multiprocessor Count: {device.MULTIPROCESSOR_COUNT}")
+    print(f"Max Threads per Block: {device.MAX_THREADS_PER_BLOCK}")
+    # print(f"Max Threads per Multiprocessor: {device.MAX_THREADS_PER_MULTIPROCESSOR}")
+    print(f"Max Block Dim X: {device.MAX_BLOCK_DIM_X}")
+    print(f"Max Block Dim Y: {device.MAX_BLOCK_DIM_Y}")
+    print(f"Max Block Dim Z: {device.MAX_BLOCK_DIM_Z}")
+    print(f"Max Grid Dim X: {device.MAX_GRID_DIM_X}")
+    print(f"Max Grid Dim Y: {device.MAX_GRID_DIM_Y}")
+    print(f"Max Grid Dim Z: {device.MAX_GRID_DIM_Z}")
+    print(f"Warp Size: {device.WARP_SIZE}")
+    print(f"Compute Capability: {device.compute_capability}")
+    print(f"Concurrent Kernels: {device.CONCURRENT_KERNELS}")
+    print(f"PCI Bus ID: {device.PCI_BUS_ID}")
+    print(f"PCI Device ID: {device.PCI_DEVICE_ID}")
+    print(f"PCI Domain ID: {device.PCI_DOMAIN_ID}")
+
