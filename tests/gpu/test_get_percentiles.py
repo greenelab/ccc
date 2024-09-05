623272681a635625061e5c598261ac4fc98c3430
Haoyu Zhang <haoyu_z@outlook.com>
2024-09-05 14:36:58 -0600

    [test]: Use all-close to take CPU-GPU FP errors into consideration

--- tests/gpu/test_get_percentiles.py
+++ tests/gpu/test_get_percentiles.py
@@ -1,16 +1,10 @@
 import pytest
-from typing import List
 
 import numpy as np
-from numpy.testing import assert_array_equal, assert_allclose
-from numpy.typing import NDArray
+from numpy.testing import assert_array_equal
 
 from ccc.coef.impl_gpu import (
     get_perc_from_k,
-    get_range_n_percentages,
-    convert_n_clusters,
-    get_range_n_clusters,
-    get_parts,
 )
 
 from ccc.coef import get_perc_from_k as get_perc_from_k_cpu
@@ -23,11 +17,10 @@ def test_get_perc_from_k_with_k_less_than_two():
 
 
 @pytest.mark.parametrize("k", [
-    # 2, 3, 4, 5, 6, 7, 8, 9, 10
-    3
+    2, 3, 4, 5, 6, 7, 8, 9, 10
 ])
 def test_get_perc_from_k(k):
     np.set_printoptions(precision=17)
     gpu_result = get_perc_from_k(k)
     cpu_result = get_perc_from_k_cpu(k)
-    assert np.equal(gpu_result, cpu_result).all()
+    assert np.allclose(gpu_result, cpu_result)
