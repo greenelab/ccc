9964cc7c2d683aee85b25d66299bb018b46ed28e
Haoyu Zhang <haoyu_z@outlook.com>
2024-10-24 14:23:09 -0600

    [kernel]: Clean up debugging code

--- libs/ccc/sklearn/metrics.cu
+++ libs/ccc/sklearn/metrics.cu
@@ -4,9 +4,7 @@
 #include <cmath>
 #include <assert.h>
 
-// #define N_OBJS 16
-// #define N_PARTS 1
-// #define N_FEATURES 2
+// Todo: Add CudaCheckError
 
 /**
  * @brief Unravel a flat index to the corresponding 2D indicis
@@ -85,18 +83,6 @@ __device__ void get_contingency_matrix(int *part0, int *part1, int n, int *share
         }
     }
     __syncthreads();
-    // print shared_cont_mat for debugging in a 2D way
-    // if (bid == 0)
-    // {
-    //     for (int i = 0; i < k; ++i)
-    //     {
-    //         for (int j = 0; j < k; ++j)
-    //         {
-    //             printf("shared_cont_mat[%d][%d]: %d\n", i, j, shared_cont_mat[i * k + j]);
-    //         }
-    //     }
-    // }
-    
 }
 
 
@@ -133,19 +119,7 @@ __device__ void get_pair_confusion_matrix(
         atomicAdd(&sum_rows[row], val);
     }
     __syncthreads();
-    // print sum_rows and sum_cols in arrays for debugging
-    // if (threadIdx.x == 0) {
-    //     printf("sum_rows:\n");
-    //     for (int i = 0; i < k; ++i) {
-    //         printf("%d ", sum_rows[i]);
-    //     }
-    //     printf("\nsum_col:\n");
-    //     for (int i = 0; i < k; ++i) {
-    //         printf("%d ", sum_cols[i]);
-    //     }
-    // }
     
-
     // Compute sum_squares
     int sum_squares;
     if (threadIdx.x == 0) {
@@ -228,35 +202,19 @@ __global__ void ari(int *parts,
     // each block is responsible for one ARI computation
     int ari_block_idx = blockIdx.x;
 
-    // print parts for debugging
-
-
     // obtain the corresponding parts and unique counts
-    // printf("n_part_mat_elems: %d\n", n_part_mat_elems);
     int feature_comp_flat_idx = ari_block_idx / n_part_mat_elems; // flat comparison pair index for two features
     int part_pair_flat_idx = ari_block_idx % n_part_mat_elems;    // flat comparison pair index for two partitions of one feature pair
     int i, j;
-
-    // if (global_tid == 0)
-    // {
-    //     printf("ari_block_idx: %d, feature_comp_flat_idx: %d, part_pair_flat_idx: %d\n", ari_block_idx, feature_comp_flat_idx, part_pair_flat_idx);
-    // }
 
     // unravel the feature indices
     get_coords_from_index(n_features, feature_comp_flat_idx, &i, &j);
     assert(i < n_features && j < n_features);
     assert(i >= 0 && j >= 0);
-    // if (global_tid == 0)
-    // {
-    //     printf("global_tid: %d, i: %d, j: %d\n", global_tid, i, j);
-    // }
+
     // unravel the partition indices
     int m, n;
     unravel_index(part_pair_flat_idx, n_parts, &m, &n);
-    // if (global_tid == 0)
-    // {
-    //     printf("global_tid: %d, m: %d, n: %d\n", global_tid, m, n);
-    // }
 
     // Make pointers to select the parts and unique counts for the feature pair
     // Todo: Use int4*?
@@ -294,22 +252,7 @@ __global__ void ari(int *parts,
      */
     // shared mem address for the contingency matrix
     int *s_contingency = shared_mem + 2 * n_objs;
-    // initialize the contingency matrix to zero
-    // const int n_contingency_items = k * k;
-    // for (int i = threadIdx.x; i < n_contingency_items; i += blockDim.x) {
-    //     s_contingency[i] = 0;
-    // }
     get_contingency_matrix(t_data_part0, t_data_part1, n_objs, s_contingency, k);
-    // if (global_tid == 0)
-    // {
-    //     for (int i = 0; i < k; ++i)
-    //     {
-    //         for (int j = 0; j < k; ++j)
-    //         {
-    //             printf("s_contingency[%d][%d]: %d\n", i, j, s_contingency[i * k + j]);
-    //         }
-    //     }
-    // }
 
     /*
      * Step 3: Construct pair confusion matrix
