cebbb242a1a2f4c60f4ba76af9872440a605aa00
Haoyu Zhang <haoyu_z@outlook.com>
2025-01-09 15:44:52 -0700

    [doc]: Initialize sphnix and read the docs

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ccc-gpu'
+copyright = '2025, Milton Pividori, Haoyu Zhang, Kevin Fotso'
+author = 'Milton Pividori, Haoyu Zhang, Kevin Fotso'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'haiku'
+html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
